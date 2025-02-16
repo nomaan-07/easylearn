b7fff1ba4a08edeec5d5adca807d507e43c38a32
Nomaan <nomaan07.dev@gmail.com>
2025-02-16 04:03:54 +0330

    Change the url

--- js/database/database-api.js
+++ js/database/database-api.js
@@ -1,8 +1,9 @@
 import '../../vendor/supabase/supabase-js@2.js';
 
 const { createClient } = supabase;
-const supabaseUrl = 'https://vqvbwalqiwdeyzuiltqm.supabase.co';
-const supabasePublicAPIKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxdmJ3YWxxaXdkZXl6dWlsdHFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE1NTI0MTQsImV4cCI6MjAzNzEyODQxNH0.gx9vDTTyesP9rVv-AiqiY8f4v970X6lzHoQ4nBlacJQ';
+const supabaseUrl = 'https://yseozmctkahaaxecwmyn.supabase.co';
+const supabasePublicAPIKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzZW96bWN0a2FoYWF4ZWN3bXluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2NTk2OTEsImV4cCI6MjA1NTIzNTY5MX0.pOF3tUTP_fN3JO-6CJ3kr1eke70CGGBs-9Uq_y2h-SQ';
+
 const client = createClient(supabaseUrl, supabasePublicAPIKey);
 
 const getAllFromDatabase = async (tableName) => {
